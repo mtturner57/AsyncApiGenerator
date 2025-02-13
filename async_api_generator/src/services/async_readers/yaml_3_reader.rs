d65d1f49e85f8486051e864d4943d82f2eba2174
Matthew Turner <mtturner57@gmail.com>
2025-02-12 21:45:46 -0500

    Update yaml_3_reader.rs

--- async_api_generator/src/services/async_readers/yaml_3_reader.rs
+++ async_api_generator/src/services/async_readers/yaml_3_reader.rs
@@ -2,8 +2,6 @@ use std::error::Error;
 
 use crate::structs::version_file_contents::VersionFileContents;
 
-
-
 pub fn createV3(contents: &VersionFileContents) -> Result<String, Box<dyn Error>>{
     Ok(String::from("test"))
 }
